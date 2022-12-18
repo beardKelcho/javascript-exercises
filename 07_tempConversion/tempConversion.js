814b27900f6569b5292c724340012c06dfc17847
beardKelcho <c.kaaan@gmail.com>
2022-12-19 01:06:38 +0300

    add to  solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  const convert = (fahrenheit - 32) / 1.8 ;
+  return round (convert,1);
 };
 
-const ctof = function() {
-
+const ctof = function(celcius) {
+  const convert = (celcius * 1.8) + 32;
+  return  round (convert,1);
 };
+
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
